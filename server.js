8f6459b643a694605672bae0552c0ee119b0defa
karina-d-larson <karina.d.larson@gmail.com>
2026-09-23 22:28:40 -0600

    push for first assignment

---
+++ server.js
@@ -0,0 +1,52 @@
+/* ================================
+   Imports
+   ================================ */
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+/* ================================
+   Important variables
+   ================================ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+
+/* ================================
+   Express server setup
+   ================================ */
+const app = express();
+
+/* ================================
+   Middleware configuration
+   ================================ */
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+
+/* ================================
+   Routes
+   ================================ */
+app.get('/', (req, res) => {
+  const title = 'Welcome Home';
+  res.render('home', { title });
+});
+
+app.get('/about', (req, res) => {
+  const title = 'About Me';
+  res.render('about', { title });
+});
+
+app.get('/products', (req, res) => {
+  const title = 'Our Products';
+  res.render('products', { title });
+});
+
+/* ================================
+   Server startup
+   ================================ */
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
